1c487a69fcf1381873dbf3b5d12083d48c6e3024
kevinhelly <kevinhelly.kh@gmail.com>
2014-10-13 14:30:28 +0200

    Add of Product.cs in ProductLib library and add of properties in said .cs file

---
+++ ProductLib/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLib
+{
+    public class Product
+    {
+        public decimal UnitPrice { get; set; }
+        public string BarCode { get; set; }
+        public string Title { get; set; }
+    }
+}
