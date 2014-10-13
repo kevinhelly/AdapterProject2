02e2f021cf3235208b1348efeea87c925c5f6413
kevinhelly <kevinhelly.kh@gmail.com>
2014-10-13 15:28:17 +0200

    Implementation of Product.cs class in UnitTest and start of SomeExternalVendorProductTest

--- AdapterTestsLib/UnitTest1.cs
+++ AdapterTestsLib/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductLib;
 
 namespace AdapterTestsLib
 {
@@ -7,16 +8,34 @@ namespace AdapterTestsLib
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ProductTest()
         {
-            
+            Product p = new Product();
+            p.Title = "Fiskdansen 2";
+            p.UnitPrice = 200;
+            p.BarCode = "|||  ||  ||||| ||";
+
+            var result = p;
+
             string expectedTitle = "Fiskdansen 2";
             decimal expectedUnitPrice = 200;
             string expectedBarCode = "|||  ||  ||||| ||";
 
-            Assert.AreEqual(expectedTitle, "Fiskdansen 2");
-            Assert.AreEqual(expectedUnitPrice, 200);
-            Assert.AreEqual(expectedBarCode, "|||  ||  ||||| ||");
+            Assert.AreEqual(expectedTitle, result.Title);
+            Assert.AreEqual(expectedUnitPrice, result.UnitPrice);
+            Assert.AreEqual(expectedBarCode, result.BarCode);
+        }
+        [TestMethod]
+        public void SomeExternalVendorProductTest()
+        {
+
+            string expectedDescription = "Fiskdansen 2";
+            int expectedPricePerUnit = 200;
+            string expectedEANCode = "|||  ||  ||||| ||";
+
+            Assert.AreEqual(expectedDescription, "");
+            Assert.AreEqual(expectedPricePerUnit, 0);
+            Assert.AreEqual(expectedEANCode, "");
         }
     }
 }
